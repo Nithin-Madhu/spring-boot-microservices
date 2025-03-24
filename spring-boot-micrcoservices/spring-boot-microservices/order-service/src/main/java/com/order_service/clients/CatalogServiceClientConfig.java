466286dd53ea1e4cdc715515bb2bb1cc744397ca
Nithin <madhunithin8@gmail.com>
2025-03-24 14:49:58 +0530

    catalogs service docker file ; order service docker file

---
+++ spring-boot-micrcoservices/spring-boot-microservices/order-service/src/main/java/com/order_service/clients/CatalogServiceClientConfig.java
@@ -0,0 +1,23 @@
+package com.order_service.clients;
+
+import java.time.Duration;
+
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+import com.order_service.ApplicationProperties;
+
+@Configuration
+class CatalogServiceClientConfig {
+    @Bean
+    RestClient restClient(RestClient.Builder builder, ApplicationProperties properties) {
+        return builder.baseUrl(properties.catalogServiceUrl())
+                .requestFactory(ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS
+                        .withConnectTimeout(Duration.ofSeconds(5))
+                        .withReadTimeout(Duration.ofSeconds(5))))
+                .build();
+    }
+}
---
+++ spring-boot-micrcoservices/spring-boot-microservices/order-service/src/main/java/com/order_service/domain/models/CreateOrderResponse.java
@@ -0,0 +1,3 @@
+package com.order_service.domain.models;
+
+public record CreateOrderResponse(String orderNumber) {}
