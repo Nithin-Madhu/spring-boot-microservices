7a89ec04a67790da71e75affa09870d7e189bf7f
Nithin <madhunithin8@gmail.com>
2025-04-08 21:03:38 +0530

    added api-gateway

---
+++ spring-boot-micrcoservices/spring-boot-microservices/catalog-service/src/main/java/com/catalog_service/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.catalog_service.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	public void addCorsMapping(CorsRegistry registry) {
+		registry.addMapping("*")
+		.allowedMethods("*")
+		.allowedHeaders("*")
+		.allowedOriginPatterns("*")
+		.allowCredentials(false);
+	}
+
+}
