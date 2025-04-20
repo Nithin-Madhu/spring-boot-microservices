cf2ec3e5c99a811d38028537aedbb0cbd4e34d80
Nithin <madhunithin8@gmail.com>
2025-04-20 16:47:20 +0530

    added security for bookstore-webapp

--- spring-boot-micrcoservices/spring-boot-microservices/bookstore-webapp/src/main/java/com/bookstore_webapp/controllers/OrderController.java
+++ spring-boot-micrcoservices/spring-boot-microservices/bookstore-webapp/src/main/java/com/bookstore_webapp/controllers/OrderController.java
@@ -1,6 +1,7 @@
 package com.bookstore_webapp.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,53 +18,60 @@ import com.bookstore_webapp.clients.orders.OrderConfirmationDTO;
 import com.bookstore_webapp.clients.orders.OrderDTO;
 import com.bookstore_webapp.clients.orders.OrderServiceClient;
 import com.bookstore_webapp.clients.orders.OrderSummary;
+import com.bookstore_webapp.services.SecurityHelper;
 
 import jakarta.validation.Valid;
 
 @Controller
-public class OrderController {
-	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
-	
+class OrderController {
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
     private final OrderServiceClient orderServiceClient;
+    private final SecurityHelper securityHelper;
 
-    OrderController(OrderServiceClient orderServiceClient){
+    OrderController(OrderServiceClient orderServiceClient, SecurityHelper securityHelper) {
         this.orderServiceClient = orderServiceClient;
+        this.securityHelper = securityHelper;
     }
 
-	@GetMapping("/cart")
-	String cart() {
-		return "cart";
-	}
-
-	@GetMapping("/orders/{orderNumber}")
-	String showOrderDetails(@PathVariable String orderNumber, Model model) {
-		model.addAttribute("orderNumber", orderNumber);
-		return "order_details";
-	}
-
-	@GetMapping("/orders")
-	String showOrders() {
-		return "orders";
-	}
-	
-    @GetMapping("/api/orders")
-    @ResponseBody
-    List<OrderSummary> getOrders() {
-        log.info("Fetching orders");
-        return orderServiceClient.getOrders();//getHeaders());
+    @GetMapping("/cart")
+    String cart() {
+        return "cart";
     }
-    
-    @GetMapping("/api/orders/{orderNumber}")
-    @ResponseBody
-    OrderDTO getOrder(@PathVariable String orderNumber) {
-        log.info("Fetching order details for orderNumber: {}", orderNumber);
-        return orderServiceClient.getOrder(orderNumber);
-    }
-    
+
     @PostMapping("/api/orders")
     @ResponseBody
     OrderConfirmationDTO createOrder(@Valid @RequestBody CreateOrderRequest orderRequest) {
         log.info("Creating order: {}", orderRequest);
-        return orderServiceClient.createOrder(orderRequest);
+        return orderServiceClient.createOrder(getHeaders(), orderRequest);
+    }
+
+    @GetMapping("/orders/{orderNumber}")
+    String showOrderDetails(@PathVariable String orderNumber, Model model) {
+        model.addAttribute("orderNumber", orderNumber);
+        return "order_details";
+    }
+
+    @GetMapping("/api/orders/{orderNumber}")
+    @ResponseBody
+    OrderDTO getOrder(@PathVariable String orderNumber) {
+        log.info("Fetching order details for orderNumber: {}", orderNumber);
+        return orderServiceClient.getOrder(getHeaders(), orderNumber);
+    }
+
+    @GetMapping("/orders")
+    String showOrders() {
+        return "orders";
+    }
+
+    @GetMapping("/api/orders")
+    @ResponseBody
+    List<OrderSummary> getOrders() {
+        log.info("Fetching orders");
+        return orderServiceClient.getOrders(getHeaders());
+    }
+
+    private Map<String, ?> getHeaders() {
+        String accessToken = securityHelper.getAccessToken();
+        return Map.of("Authorization", "Bearer " + accessToken);
     }
 }
